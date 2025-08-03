d247bce6f21fe2c1a31b7e32a51e1f211ced3793
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2025-08-03 14:03:33 -0500

    initial setup with rando text on home page

---
+++ app/home/home.tsx
@@ -0,0 +1,30 @@
+
+
+export function Home() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4 px-4">
+      <div className="flex justify-between gap-2 items-center w-full">
+        <div className="text-3xl font-light">
+          Alfred Madere
+        </div>
+        <div className="max-w-[500px]">
+          <p>Engineer based in San Francisco. Entrepreneur at heart. Focused on tackling the engineering challenges of AI and scalable, data-intensive applications.</p>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </main>
+  );
+}
+
+const projects = [
+  {
+    name: "",
+    description: "",
+    image: "",
+    link: "",
+    dateRange: "",
+  }
+]
+
