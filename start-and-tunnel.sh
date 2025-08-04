c169149b85cd03d5e82d2e702b4203a4caeb241d
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2025-08-03 19:05:07 -0500

    using ssh private key as environment variable

--- start-and-tunnel.sh
+++ start-and-tunnel.sh
@@ -5,8 +5,13 @@ set -e
 
 # Start the SSH tunnel in the background
 # The -f flag sends ssh to the background. -N means do not execute a remote command.
+KEY_FILE="/root/.ssh/id_rsa"
 echo "Starting SSH tunnel..."
-ssh -o StrictHostKeyChecking=no -f -N -i /root/.ssh/id_rsa -R 8085:localhost:3000 ssh-tunnel@54.71.13.19
+echo "Storing ssh key in $KEY_FILE"
+touch $KEY_FILE
+chmod 600 $KEY_FILE
+echo "$SSH_PRIVATE_KEY" > $KEY_FILE
+ssh -o StrictHostKeyChecking=no -f -N -i $KEY_FILE -R 8085:localhost:3000 ssh-tunnel@54.71.13.19
 
 # Start the web server in the foreground
 echo "Starting web server..."
