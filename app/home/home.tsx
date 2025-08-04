6ed86a868e190582a7a87e76f1e94b7ebaad50ae
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2025-08-04 18:57:46 -0500

    added background dots

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,8 +1,9 @@
-
+import { DotPattern } from "../components/ui/dot-pattern";
 
 export function Home() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4 px-4">
+    <main className="h-screen flex items-center justify-center pt-16 pb-4 px-4 relative">
+      <DotPattern />
       <div className="flex justify-between gap-2 items-center w-full">
         <div className="text-3xl font-light">
           Alfred Madere
@@ -12,7 +13,6 @@ export function Home() {
         </div>
       </div>
       <div>
-
       </div>
     </main>
   );
