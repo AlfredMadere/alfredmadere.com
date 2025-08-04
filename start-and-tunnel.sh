c702ea2770a5e1f055521c958f35400d45da6a7a
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2025-08-03 19:18:09 -0500

    fixed using env var for secret

--- start-and-tunnel.sh
+++ start-and-tunnel.sh
@@ -10,7 +10,7 @@ echo "Starting SSH tunnel..."
 echo "Storing ssh key in $KEY_FILE"
 touch $KEY_FILE
 chmod 600 $KEY_FILE
-echo "$SSH_PRIVATE_KEY" > $KEY_FILE
+echo "$PRIV_KEY" > $KEY_FILE
 ssh -o StrictHostKeyChecking=no -f -N -i $KEY_FILE -R 8085:localhost:3000 ssh-tunnel@54.71.13.19
 
 # Start the web server in the foreground
